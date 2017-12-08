b1daf14671ff981a8f5bfd8611a977d3d0f40121
Jared <jaredjkim@gmail.com>
2017-12-07 22:58:30 -0800

    first commit

---
+++ RainbowRoad/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  RainbowRoad
+//
+//  Created by Jared K on 11/6/17.
+//  Copyright © 2017 Jared K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let colors:[UIColor] = [#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.9995340705, green: 0.988355577, blue: 0.4726552367, alpha: 1), #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)] //araay set colors
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count //one section of colors.
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+}
